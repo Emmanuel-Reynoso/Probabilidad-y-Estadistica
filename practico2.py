b6c05cf64d6f8fe7fa786666548e7c996c70713b
maed <mauro.e.bordon@gmail.com>
2020-01-13 06:44:36 -0300

    i can see that you completed it

--- practico2.py
+++ practico2.py
@@ -22,3 +22,12 @@ def varianza(x):
 def desvio(x):
 	return np.sqrt(varianza(x))
 
+def ej13():
+
+	prob_def = 1 - 0.86**(1/25)
+
+	print("* a) P(D) = 1 - 0.86**(1/25) =", "%.3f" %prob_def)
+
+	prob_10 = 1 - 0.90**(1/25)
+
+	print("* b) P(D) = 1 - 0.90**(1/25) =", "%.3f" %prob_10)
